70b2c1c84b16d5a5a5808c184db54288275de894
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-09-16 13:37:25 +0000

    Añade la clase Persona con atributos y métodos para gestionar el estado de los clientes en la simulación.

---
+++ entregas/rafaelKaterine/src/Persona.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Persona {
+
+    public boolean esPreferencial;
+    public int minutoDeLlegada;
+    private static final Random random = new Random();
+
+    public Persona(int minutoDeLlegada) {
+        this.esPreferencial = random.nextBoolean() < 0.15;
+        this.minutoDeLlegada = minutoDeLlegada;
+    } 
+
+    public boolean seAburre (int minutoActual) {
+        int tiempoDeEspera = minutoActual - minutoDeLlegada;
+        if (tiempoDeEspera >= 8 && minutoActual % 5 == 0){
+            return random.nextBoolean() < 0.30;
+        }
+        return false;
+    }
+
+
+
+
+}
