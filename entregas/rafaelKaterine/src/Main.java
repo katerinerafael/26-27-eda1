df6e5a5ace4ce9fbfd0417a69e1e40fdff343eaf
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-09-16 11:30:44 +0000

    Agregacion de clases Caja, Cliente y Cola de codigos pasados

---
+++ entregas/rafaelKaterine/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CentroComercial centroComercial = new CentroComercial(9, 21, 60, 4, 5, 15);
+        centroComercial.simularJornada();
+        centroComercial.mostrarResumen();
+    }
+}
