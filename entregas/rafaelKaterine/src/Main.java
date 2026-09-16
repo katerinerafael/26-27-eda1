ed8bde644911a9ffa9cc1e8e3bf1637f4b825b6b
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-09-16 13:23:36 +0000

    Elimina las clases Caja, Cliente y Cola, y agrega un README con el planteamiento inicial del proyecto y las reglas de simulación.

--- entregas/rafaelKaterine/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CentroComercial centroComercial = new CentroComercial(9, 21, 60, 4, 5, 15);
-        centroComercial.simularJornada();
-        centroComercial.mostrarResumen();
-    }
-}
